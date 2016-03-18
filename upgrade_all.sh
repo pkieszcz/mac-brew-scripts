1933b5e4418bcd3618f57c5a16a48297deb6c2b5
Piotr Kieszczyński <pkieszczynski@MacBook-Pro-Piotr.local>
2016-03-18 11:57:19 +0100

    Fixed upgrade script - it actually does work right now

--- upgrade_all.sh
+++ upgrade_all.sh
@@ -5,5 +5,18 @@ brew upgrade --all
 
 brew cask update
 
+casks=( $(brew cask list) )
+
+for cask in ${casks[@]}
+do
+    # in the first line there is version
+    current="$(brew cask info $cask | sed -n '1p' | sed -n 's/^.*: \(.*\)$/\1/p')"
+
+    installed=( $(ls /opt/homebrew-cask/Caskroom/$cask))
+    if (! [[ " ${installed[@]} " == *" $current "* ]]); then
+        (set -x; brew cask install $cask --force;)
+    fi
+done
+
 brew cleanup
 brew cask cleanup
