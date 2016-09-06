5bb26d970889204befad8bc2572d6bab3bbf62cb
Piotr Kieszczyński <p.kieszczynski@oberthur.com>
2016-09-06 10:16:29 +0200

    Updated to my current needs

--- install_cask_and_apps.sh
+++ install_cask_and_apps.sh
@@ -2,44 +2,43 @@
 
 brew install caskroom/cask/brew-cask
 brew tap caskroom/fonts
+brew tap colindean/fonts-nonfree
 brew cask install font-inconsolata
 brew cask install font-open-sans
 brew cask install font-oswald
 
 # Apps
 apps=(
-	airmail-beta
-	android-file-transfer
-	appcleaner
-	atom
-	cakebrew
-	ccleaner
-	clipmenu
-	evernote
-	firefox
-	flash
-	flux
-	font-inconsolata
-	font-open-sans
-	font-oswald
-	google-chrome
-	gpgtools
-	Caskroom/versions/iterm2-beta
-	itsycal
-	keepassx
-	osxfuse
-	skype
-	slack
-	sourcetree
-	spectacle
-	spotify
-	teamviewer
-	Caskroom/cask/tomighty
-	tunnelblick
-	vagrant
-	virtualbox
-	wireshark
-	xquartz
+  appcleaner
+  atom
+  cakebrew
+  clipmenu
+  docker
+  evernote
+  firefox
+  flux
+  font-inconsolata
+  font-microsoft-office
+  font-open-sans
+  font-oswald
+  google-chrome
+  gpgtools
+  iterm2-beta
+  itsycal
+  java
+  karabiner
+  keepassx
+  skype
+  slack
+  sourcetree
+  spectacle
+  spotify
+  teamviewer
+  tomighty
+  tunnelblick
+  vagrant
+  virtualbox
+  wireshark
 )
 
 # Default is: /Users/$user/Applications
