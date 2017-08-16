d5f0530fdb5647c52880a951a0d674124516b7ec
Piotr Kieszczyński <piotr.kieszczynski@gmail.com>
2017-08-16 12:05:04 +0200

    Make it somehow usable

--- make_mac_suck_less.sh
+++ make_mac_suck_less.sh
@@ -24,6 +24,9 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 echo "Remove the auto-hiding dock delay."
 defaults write com.apple.dock autohide-delay -int 0
 
+echo "Set dock size to 32px"
+defaults write com.apple.dock tilesize -int 32
+
 echo "Use the dark theme."
 defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle -string "Dark"
 
@@ -83,18 +86,21 @@ defaults write DoNotOfferNewDisksForBackup com.apple.TimeMachine -int 1
 echo "Disable natural scrolling."
 defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
 
+echo "Enable Developer Tools for Safari"
+defaults write com.apple.Safari WebKitDeveloperExtras -bool true
+
 # Security And Privacy Improvements
 echo "Disable Safari from auto-filling sensitive data."
 defaults write ~/Library/Preferences/com.apple.Safari AutoFillCreditCardData -bool false
 defaults write ~/Library/Preferences/com.apple.Safari AutoFillFromAddressBook -bool false
 defaults write ~/Library/Preferences/com.apple.Safari AutoFillMiscellaneousForms -bool false
-defaults write ~/Library/Preferences/com.apple.Safari AutoFillPasswords -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari AutoFillPasswords -bool false
 
 echo "Disable Safari from automatically opening files."
 defaults write ~/Library/Preferences/com.apple.Safari AutoOpenSafeDownloads -bool false
 
-echo "Always block cookies and local storage in Safari."
-defaults write ~/Library/Preferences/com.apple.Safari BlockStoragePolicy -bool false
+#echo "Always block cookies and local storage in Safari."
+#defaults write ~/Library/Preferences/com.apple.Safari BlockStoragePolicy -bool false
 
 echo "Enable Safari warnings when visiting fradulent websites."
 defaults write ~/Library/Preferences/com.apple.Safari WarnAboutFraudulentWebsites -bool true
@@ -107,14 +113,14 @@ echo "Block popups in Safari."
 defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
 defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
 
-echo "Disable plugins and extensions in Safari."
-defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2WebGLEnabled -bool false
-defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
-defaults write ~/Library/Preferences/com.apple.Safari WebKitPluginsEnabled -bool false
-defaults write ~/Library/Preferences/com.apple.Safari ExtensionsEnabled -bool false
-defaults write ~/Library/Preferences/com.apple.Safari PlugInFirstVisitPolicy PlugInPolicyBlock
-defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
-defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaEnabled -bool false
+#echo "Disable plugins and extensions in Safari."
+#defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2WebGLEnabled -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari WebKitPluginsEnabled -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari ExtensionsEnabled -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari PlugInFirstVisitPolicy PlugInPolicyBlock
+#defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
+#defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaEnabled -bool false
 
 echo "Safari should treat SHA-1 certificates as insecure."
 defaults write ~/Library/Preferences/com.apple.Safari TreatSHA1CertificatesAsInsecure -bool true
